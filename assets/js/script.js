5657661e10bced76d0704cbd82fea9ffa29dcc23
john65 <joaovini2667@gmail.com>
2023-02-22 16:10:09 -0300

    uptade coomments, html - css - js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
+//Variables DOM
 const fileInput = document.querySelector("input"),
 downloadBtn = document.querySelector("button");
 
+//Function will be called when the button is clicked
 downloadBtn.addEventListener("click", e => {
     e.preventDefault();
     downloadBtn.innerText = "Downloading File...";
     fetchFile(fileInput.value);
 });
 
+//This function below has the function of taking the URL of the input and downloading the value of the URL
 function fetchFile(url){
     fetch(url).then(res => res.blob()).then(file => {
         let tempUrl = URL.createObjectURL(file);
@@ -20,6 +23,7 @@ function fetchFile(url){
         URL.revokeObjectURL(tempUrl);
         downloadBtn.innerText = "Download File";
     }).catch(() => {
+        //If the download does not go as expected, an error message will appear.
         downloadBtn.innerText = "Download File";
         alert("Failed to download file");
     })
